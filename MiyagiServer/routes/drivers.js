c624ec8339c0b8ca235a34361de0babefe1f9bcd
Jonas Hinn <jonas.hinn@samsung.com>
2016-02-26 23:39:42 -0800

    Get all drivers

--- MiyagiServer/routes/drivers.js
+++ MiyagiServer/routes/drivers.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Driver = mongoose.model('Driver');
 
-/* GET users listing. */
+/* GET all drivers listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	Driver.find({}, function(err, drivers) {
+	    if (!err) { 
+	        res.json(200, drivers);
+	    } else {
+	    	throw err;
+	    }
+	});
 });
 
 module.exports = router;
