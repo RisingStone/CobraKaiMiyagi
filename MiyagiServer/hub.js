6975caed9cf45dd186a76aa5614acaa35a21ec12
Jonas Hinn <jonas.hinn@samsung.com>
2016-02-27 11:02:56 -0800

    Adding basic hub model to the db.

---
+++ MiyagiServer/hub.js
@@ -0,0 +1,21 @@
+// import the necessary modules
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// model creation
+var HubModel = function() {
+  var HubSchema = new Schema({
+    name: String,
+    location: {
+    	type: {
+    		type: String
+    	},
+    	coordinates: [Number]
+    }
+  });
+  // register the mongoose model
+  mongoose.model('Hub', DriverSchema);
+};
+
+// create an export function to encapsulate the model creation
+module.exports = HubModel;
--- MiyagiServer/routes/drivers.js
+++ MiyagiServer/routes/drivers.js
@@ -5,13 +5,38 @@ var Driver = mongoose.model('Driver');
 
 /* GET all drivers listing. */
 router.get('/', function(req, res, next) {
-	Driver.find({}, function(err, drivers) {
-	    if (!err) { 
-	        res.json(200, drivers);
-	    } else {
-	    	throw err;
-	    }
-	});
+	var limit = req.query.limit || 100;
+
+	if(!req.query || !req.query.longitude || !req.query.latitude) {
+		Driver.find({}).limit(limit).exec(function(err, drivers) {
+		    if (!err) { 
+		        res.json(200, drivers);
+		    } else {
+		    	throw err;
+		    }
+		});
+	} else {
+	    var max = req.query.distance || 100;
+
+	    // Max distance to radians: radius of earth is approx 6371km.
+	    max /= 6371;
+
+	    // long, lat coordinates (notice order!)
+	    var coords = [];
+	    coords[0] = req.query.longitude;
+	    coords[1] = req.query.latitude;
+
+	    // get nearby drivers
+	    Driver.find({
+	      location: {
+	        $near: coords,
+	        $maxDistance: maxDistance
+	      }
+	    }).limit(limit).exec(function(err, drivers) {
+	      if (err) return res.json(500, err);
+	      res.json(200, drivers);
+	    });
+	}
 });
 
 module.exports = router;
