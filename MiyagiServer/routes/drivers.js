4dd44bc7eff10df87ad880a54208a9d1c3bd68d7
Jonas Hinn <jonas.hinn@samsung.com>
2016-02-27 15:41:36 -0800

    Create new driver from json body.

--- MiyagiServer/routes/drivers.js
+++ MiyagiServer/routes/drivers.js
@@ -42,4 +42,12 @@ router.get('/', function(req, res, next) {
 	}
 });
 
+router.post('/', function(req, res, next) {
+	var newDriver = new Driver(req.body);
+	newDriver.save(function(err) {
+		if(err) res.send(500, err);
+		else res.json(req.body);
+	});
+});
+
 module.exports = router;
