885b5b199207613eced4335c723899a97794e66d
Jonas Hinn <jonas.hinn@samsung.com>
2016-02-26 23:18:09 -0800

    Adding static driver data for static hub

--- MiyagiServer/app.js
+++ MiyagiServer/app.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var async = require('async');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var driver = require('./driver')();
+var Drivers = mongoose.model('Driver');
 
+var fs = require('fs');
+var path = require('path');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var drivers = require('./routes/drivers');
+
+// Bootstrap mongoose and load dummy data
+mongoose.connect('mongodb://localhost/miyagi_db', function(err) {
+  if (err) throw err;
+
+  // load data from file and transform it to Object
+  var data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8'));
+
+  // clean db and load new data
+  Drivers.remove(function() {
+    async.each(data, function(item, callback) {
+      // create a new location
+      Drivers.create(item, callback);
+    }, function(err) {
+      if (err) throw err;
+    });
+  });
+
+});
 
 var app = express();
 
@@ -23,7 +48,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/drivers', drivers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
